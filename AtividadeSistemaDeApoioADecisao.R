60730b56cc6c1813c0c25bc550fa5b46e70526e4
Alex Jordan <alex.jordan@ucsal.edu.br>
2024-09-18 19:36:30 -0300

    Verificar quais vendedores atingiram a meta

--- AtividadeSistemaDeApoioADecisao.R
+++ AtividadeSistemaDeApoioADecisao.R
@@ -1,2 +1,7 @@
 # Criar vetor de vendas 
 vendas <- c(18, 22, 14, 25, 19, 16, 12, 23, 17, 20)
+
+# Verificar quais vendedores atingiram a meta 
+meta_minima <- 20
+vendedores_meta <- vendas >= meta_minima
+
